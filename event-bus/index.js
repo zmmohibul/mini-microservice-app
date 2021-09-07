572e576f008996092e4116acc943704268719cb3
zmmohibul <18-36427-1@student.aiub.edu>
2021-09-07 17:17:27 +0600

    some commit

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,18 +4,26 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
+const events = [];
+
 
 app.post('/events', (req, res) => {
     const event = req.body;
 
+    events.push(event);
     console.log(event);
 
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
+    axios.post('http://posts-clusterip-srv:4000/events', event);
+    axios.post('http://comments-srv:4001/events', event);
+    axios.post('http://query-srv:4002/events', event);
+    axios.post('http://moderation-srv:4003/events', event);
 
     res.send({ status: 'OK' });
 });
+
+app.get('/events', (req, res) => {
+    res.send(events);
+})
 
 app.listen(4005, () => {
     console.log('Listening on port 4005');
--- query/index.js
+++ query/index.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
- 
+const axios = require('axios');
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 const posts = {};
+
+const handleEvent = (type, data) => {
+    if (type === 'PostCreated') {
+        const { id, title } = data;
+
+        posts[id] = { id, title, comments: [] };
+    }
+
+    if (type === 'CommentCreated') {
+        const { id, content, postId, status } = data;
+
+        const post = posts[postId];
+        post.comments.push({ id, content, status });
+    }
+
+    if (type === 'CommentUpdated') {
+        const { id, content, postId, status } = data;
+
+        const post = posts[postId];
+        const comment = post.comments.find(comment => {
+            return comment.id == id;
+        });
+
+        comment.status = status;
+        comment.content = content;
+    }
+}
+
 
 app.get('/posts', (req, res) => {
     res.send(posts);
@@ -15,24 +44,21 @@ app.get('/posts', (req, res) => {
 app.post('/events', (req, res) => {
     const { type, data } = req.body;
 
-    if (type === 'PostCreated') {
-        const { id, title } = data;
-
-        posts[id] = { id, title, comments: [] };
-    }
-
-    if (type === 'CommentCreated') {
-        const { id, content, postId } = data;
-
-        const post = posts[postId];
-        post.comments.push({ id, content });
-    }
+    handleEvent(type, data);
 
     console.log(posts)
 
     res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log('Listening on Port 4002 ');
+
+    const res = await axios.get('http://event-bus-srv:4005/events');
+
+    for (let event of res.data) {
+        console.log('Processing event', event.type);
+
+        handleEvent(event.type, event.data);
+    }
 })
